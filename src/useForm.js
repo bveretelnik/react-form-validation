799bd2379518a2903ddcec38188b483ef44d738f
Bogdan <bveretelnik@gmail.com>
2020-10-11 16:50:48 +0300

    upload file

---
+++ src/useForm.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react';
+
+export const useForm = (callback,validate) => {
+        const [values,setValue] = useState({
+                username: '',
+                email: '',
+                password: '',
+                password2: ''
+            })
+
+        const [errors, setErrors] = useState({})
+        const [isSubmitting, setIsSubmitting] = useState(false)
+
+
+    const handleChange = e => {
+        const {name,value} = e.target
+        setValue({
+            ...values,
+            [name]:value
+        })
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+
+        setErrors(validate(values))
+        setIsSubmitting(true)
+    }
+    useEffect(
+        () => {
+        if(Object.keys(errors).length === 0 && isSubmitting) {
+            callback()
+        }
+    }, [errors])
+
+    return {handleChange, handleSubmit, values, errors}
+}
